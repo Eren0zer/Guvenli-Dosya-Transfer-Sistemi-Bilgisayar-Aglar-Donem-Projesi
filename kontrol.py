f766f0677c559b62ca6bdc717fa212906c752887
Eren Özer <146471616+Eren0zer@users.noreply.github.com>
2025-06-08 23:28:08 +0300

    Delete kontrol.py

--- kontrol.py
+++
@@ -1,5 +0,0 @@
-with open("example.txt", "rb") as f1, open("received_example.txt", "rb") as f2:
-    if f1.read() == f2.read():
-        print("✅ Dosyalar tamamen aynı!")
-    else:
-        print("❌ Dosyalar farklı!")
